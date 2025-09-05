3f9e560f92c4bf49c07a129d05eb18b5bce79551
Ritcher Code <ritcher.recomienda@gmail.com>
2025-09-05 09:59:40 -0400

    Update watcher.js

--- src/watcher.js
+++ src/watcher.js
@@ -1,16 +1,9 @@
-// src/watcher.js
-// Controlador para cambiar entre poses en Veadotube Mini
-
 const fs = require("fs");
 const path = require("path");
 
-// Carpeta de assets
 const assetsDir = path.join(__dirname, "../assets");
-
-// Archivo "current.png" que Veadotube Mini usará como fuente
 const outputPath = path.join(assetsDir, "current.png");
 
-// Función genérica para cambiar a una pose
 function setPose(pose) {
   const posePath = path.join(assetsDir, `${pose}.png`);
   if (fs.existsSync(posePath)) {
@@ -21,11 +14,10 @@ function setPose(pose) {
   }
 }
 
-// Exportamos las funciones
 module.exports = {
-  setIdle: () => setPose("idle"),
-  setTalking: () => setPose("talking"),
-  setHappy: () => setPose("happy"),
-  setAngry: () => setPose("angry"),
-  setSad: () => setPose("sad"),
+  setIdle:   () => setPose("idle"),
+  setTalking:() => setPose("talking"),
+  setHappy:  () => setPose("happy"),
+  setAngry:  () => setPose("angry"),
+  setSad:    () => setPose("sad"),
 };
